8511aeeae7c606689c4181cd0fb24fa7cffb2e37
lycho33 <cho.lyds@gmail.com>
2021-02-08 11:54:41 -0500

    commit tree beef9150195ca2db28fe258b15b080d73984d368

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(languages)
+    list = []
+    counter = 0
+    while counter < languages.length
+        list << yield(languages[counter])
+        counter += 1
+    end
+    list
+end    
